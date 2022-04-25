533c48fd1e460f5b4c940042ae0a0dee83d56d93
Matteo Franci <mttfranci@gmail.com>
2022-04-25 22:28:42 +0200

    Handling of paths and schema extensions

--- src/main/java/org/fugerit/java/yaml/doc/maven/MojoGenerate.java
+++ src/main/java/org/fugerit/java/yaml/doc/maven/MojoGenerate.java
@@ -1,18 +1,12 @@
 package org.fugerit.java.yaml.doc.maven;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.util.Properties;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.fugerit.java.core.lang.helpers.StringUtils;
-import org.fugerit.java.core.util.collection.ListMapStringKey;
 import org.fugerit.java.yaml.doc.YamlDocMain;
-import org.fugerit.java.yaml.doc.config.OpenapiConfig;
-import org.fugerit.java.yaml.doc.config.YamlDocCatalog;
 
 @Mojo( name = "generate")
 public class MojoGenerate extends AbstractMojo {
@@ -23,38 +17,14 @@ public class MojoGenerate extends AbstractMojo {
     @Parameter(property = "idCatalog", required = true )
     private String idCatalog;
     
-    
-    private void addIfNotEmpty( Properties props, String key, String value ) {
-    	if ( StringUtils.isNotEmpty( value ) ) {
-    		props.setProperty( key , value );
-    	}
-    }
-    
     public void execute() throws MojoExecutionException {
         try {
-        	FileInputStream fis = new FileInputStream( new File( this.configPath ) );
-        	try {
-        		YamlDocCatalog config = new YamlDocCatalog();
-        		config = (YamlDocCatalog)YamlDocCatalog.load( fis , config );
-        		this.getLog().info( "keys : "+config.getIdSet() );
-        		ListMapStringKey<OpenapiConfig> catalog = config.getListMap( this.idCatalog );
-        		for ( OpenapiConfig current : catalog ) {
-        			Properties props = new Properties();
-                	this.addIfNotEmpty(props, YamlDocMain.ARG_INPUT_YAML, current.getInputYaml() );
-                	this.addIfNotEmpty(props, YamlDocMain.ARG_OUTPUT_FILE, current.getOutputFile() );
-                	this.addIfNotEmpty(props, YamlDocMain.ARG_LANGUAGE, current.getLanguage() );
-                	this.addIfNotEmpty(props, YamlDocMain.ARG_LABEL_OVVERRIDE, current.getLabelsOverride() );
-                	this.addIfNotEmpty(props, YamlDocMain.ARG_EXCEL_TRY_AUTORESIZE, current.getExcelTryAutoresize() );
-                	getLog().info( "using parameters -> "+props );
-                	YamlDocMain.worker( props );
-        		}
-        	} catch (Exception e) {
-        		e.printStackTrace();
-        		throw new MojoExecutionException( "Error generating code : "+e, e );	
-        	} finally {
-        		fis.close();
-			}
-        	
+        	Properties props = new Properties();
+        	props.setProperty( YamlDocMain.ARG_MODE , YamlDocMain.ARG_MODE_CONFIG );
+        	props.setProperty( YamlDocMain.ARG_CONFIG_PATH , this.configPath );
+        	props.setProperty( YamlDocMain.ARG_ID_CATALOG , this.idCatalog );
+        	this.getLog().info( "props : "+props );
+        	YamlDocMain.worker( props );
         } catch (Exception e) {
         	throw new MojoExecutionException( "Error generating code : "+e, e );
         }
